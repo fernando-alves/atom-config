cc2210e0e3fac4a363c3b165719a95592c2ddc3a
Fernando <fernandoasfj@gmail.com>
2017-05-22 08:09:34 -0300

    Install packages as part of setup

--- setup.sh
+++ setup.sh
@@ -9,3 +9,9 @@ fi;
 
 ln -sf $BASE_DIR/config.cson ~/.atom/config.cson
 ln -sf $BASE_DIR/keymap.cson ~/.atom/keymap.cson
+
+if [[ $(which apm) ]]; then
+  while read package ; do
+    apm install $package
+  done < $BASE_DIR/packages
+fi;
